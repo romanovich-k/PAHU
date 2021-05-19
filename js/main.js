79b468f596a3182fc04947ea01b6b2d4743dd752
romanovich-k <81236887+romanovich-k@users.noreply.github.com>
2021-05-19 13:12:22 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -60,20 +60,6 @@ $('.nav__body').click(function(){
 
 
 
-//Спойлеры
-
-$(document).ready(function() {
-  $('.block__title').click(function(event) {
-    if($('.block').hasClass('one')){
-      $('.block__title').not($(this)).removeClass('active');
-      $('.block__content').not($(this).next()).slideUp(300);
-    }
-    $(this).toggleClass('active').next().slideToggle(300);
-  });
-});
-
-
-
 
 //медленный скролл
 // Найти все ссылки начинающиеся на #
@@ -94,61 +80,21 @@ for(let anchor of anchors) {
 }
 
 //button to top
-const backToTopButton = document.querySelector("#back-to-top-btn");
-
-window.addEventListener("scroll", scrollFunction);
-
-function scrollFunction() {
-  if (window.pageYOffset > 300) { // Show backToTopButton
-    if(!backToTopButton.classList.contains("btnEntrance")) {
-      backToTopButton.classList.remove("btnExit");
-      backToTopButton.classList.add("btnEntrance");
-      backToTopButton.style.display = "block";
-    }
-  }
-  else { // Hide backToTopButton
-    if(backToTopButton.classList.contains("btnEntrance")) {
-      backToTopButton.classList.remove("btnEntrance");
-      backToTopButton.classList.add("btnExit");
-      setTimeout(function() {
-        backToTopButton.style.display = "none";
-      }, 250);
-    }
-  }
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if($(window).scrollTop()>300){
+    $('.scroll-top').fadeIn(250);
 }
-
-backToTopButton.addEventListener("click", smoothScrollBackToTop);
-
-// function backToTop() {
-//   window.scrollTo(0, 0);
-// }
-
-function smoothScrollBackToTop() {
-  const targetPosition = 0;
-  const startPosition = window.pageYOffset;
-  const distance = targetPosition - startPosition;
-  const duration = 750;
-  let start = null;
-  
-  window.requestAnimationFrame(step);
-
-  function step(timestamp) {
-    if (!start) start = timestamp;
-    const progress = timestamp - start;
-    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
-    if (progress < duration) window.requestAnimationFrame(step);
-  }
+else{
+    $('.scroll-top').fadeOut(250);
 }
-
-function easeInOutCubic(t, b, c, d) {
-  t /= d/2;
-  if (t < 1) return c/2*t*t*t + b;
-  t -= 2;
-  return c/2*(t*t*t + 2) + b;
-};
-
-
-
+  });
+  $('.scroll-top').click(function(){
+$('html,body').animate(
+    {scrollTop:0},400
+);
+  });
+});
 
 
 
